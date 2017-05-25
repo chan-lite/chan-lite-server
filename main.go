01e365cf407807978be8cd1105c15eaf80111561
Evan Jones <evanjones4040@gmail.com>
2017-05-24 21:46:19 -0700

    removing bits from main.go

--- main.go
+++ main.go
@@ -5,16 +5,8 @@ import (
 )
 
 func main() {
-	// We no longer need this as we're using
-	// Heroku locally.
-	// environmentError := godotenv.Load()
-	// if environmentError != nil {
-		// This is actually not an error
-		// if we're on the Heroku server
-		// for obvious reasons.
-	// }
-	routerError := router.Router()
-	if routerError != nil {
-		panic(routerError)
+	err := router.Router()
+	if err != nil {
+		panic(err)
 	}
 }
